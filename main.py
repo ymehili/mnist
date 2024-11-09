36f6fdd82f29f901e0fa43f50b152aa276ff98e6
ymehili <youssefmehili@gmail.com>
2024-11-09 17:09:33 +0100

    implement main function to process MNIST dataset and activate neuron layers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+from neuron import *
 # Load the dataset
 (img_train, result_train), (img_test, result_test) = tf.keras.datasets.mnist.load_data()
 
@@ -7,6 +7,27 @@ import tensorflow as tf
 img_train, img_test = img_train / 255.0, img_test / 255.0
 
 def main():
+    for (img, result) in zip(img_train, result_train):
+        input_neurons = [Neuron([0.0], 0.0, relu) for _ in range(784)]
+        hidden_neurons1 = [Neuron([0.0 for _ in range(784)], 0.0, relu) for _ in range(128)]
+        hidden_neurons2 = [Neuron([0.0 for _ in range(128)], 0.0, relu) for _ in range(64)]
+        output_neurons = [Neuron([0.0 for _ in range(64)], 0.0, softmax) for _ in range(10)]
+
+        # Flatten the 28x28 image to a 784x1 vector
+        inputs = img.flatten()
+
+        # Activate the input layer
+        inputs = [neuron.activate(inputs) for neuron in input_neurons]
+
+        # Activate the first hidden layer
+        inputs = [neuron.activate(inputs) for neuron in hidden_neurons1]
+
+        # Activate the second hidden layer
+        inputs = [neuron.activate(inputs) for neuron in hidden_neurons2]
+
+        # Activate the output layer
+        result = [neuron.activate(inputs) for neuron in output_neurons]
+
     return
 
 if __name__ == "__main__":
