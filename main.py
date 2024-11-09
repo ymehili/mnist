8db3c91210a9eb3594d7cbeed12b23c745307c3a
ymehili <youssefmehili@gmail.com>
2024-11-09 17:13:23 +0100

    update output layer neurons to use ReLU activation and apply softmax for predictions

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from neuron import *
 # Normalize the pixel values to [0, 1]
 img_train, img_test = img_train / 255.0, img_test / 255.0
 
+
 def main():
     for (img, result) in zip(img_train, result_train):
         input_neurons = [Neuron([0.0], 0.0, relu) for _ in range(784)]
         hidden_neurons1 = [Neuron([0.0 for _ in range(784)], 0.0, relu) for _ in range(128)]
         hidden_neurons2 = [Neuron([0.0 for _ in range(128)], 0.0, relu) for _ in range(64)]
-        output_neurons = [Neuron([0.0 for _ in range(64)], 0.0, softmax) for _ in range(10)]
+        output_neurons = [Neuron([0.0 for _ in range(64)], 0.0, relu) for _ in range(10)]
 
         # Flatten the 28x28 image to a 784x1 vector
         inputs = img.flatten()
@@ -25,8 +26,12 @@ def main():
         # Activate the second hidden layer
         inputs = [neuron.activate(inputs) for neuron in hidden_neurons2]
 
-        # Activate the output layer
-        result = [neuron.activate(inputs) for neuron in output_neurons]
+        inputs = [neuron.activate(inputs) for neuron in output_neurons]
+
+        outputs = softmax(inputs)
+
+        print(f"Predicted: {outputs.index(max(outputs))}, Actual: {result}")
+
 
     return
 
