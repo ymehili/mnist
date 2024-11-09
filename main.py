4dbf6efe4f6b9ed7a6a74aaddfb8d5adaf184392
ymehili <youssefmehili@gmail.com>
2024-11-09 17:45:22 +0100

    refactor main function to initialize neurons with random weights and improve output logging

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import tensorflow as tf
+from random import random
 from neuron import *
+
 # Load the dataset
 (img_train, result_train), (img_test, result_test) = tf.keras.datasets.mnist.load_data()
 
 # Normalize the pixel values to [0, 1]
 img_train, img_test = img_train / 255.0, img_test / 255.0
 
-
 def main():
+    input_neurons = [Neuron([(random() % 100) / 100], (random() % 100) / 100, relu) for _ in range(784)]
+    hidden_neurons1 = [Neuron([(random() % 100) / 100 for _ in range(784)], (random() % 100) / 100, relu) for _ in range(16)]
+    hidden_neurons2 = [Neuron([(random() % 100) / 100 for _ in range(16)], (random() % 100) / 100, relu) for _ in range(16)]
+    output_neurons = [Neuron([(random() % 100) / 100 for _ in range(16)], (random() % 100) / 100, relu) for _ in range(10)]
+    
     for (img, result) in zip(img_train, result_train):
-        input_neurons = [Neuron([0.0], 0.0, relu) for _ in range(784)]
-        hidden_neurons1 = [Neuron([0.0 for _ in range(784)], 0.0, relu) for _ in range(128)]
-        hidden_neurons2 = [Neuron([0.0 for _ in range(128)], 0.0, relu) for _ in range(64)]
-        output_neurons = [Neuron([0.0 for _ in range(64)], 0.0, relu) for _ in range(10)]
-
         # Flatten the 28x28 image to a 784x1 vector
         inputs = img.flatten()
 
@@ -30,9 +31,8 @@ def main():
 
         outputs = softmax(inputs)
 
-        print(f"Predicted: {outputs.index(max(outputs))}, Actual: {result}")
-
-
+        print(outputs)
+    
     return
 
 if __name__ == "__main__":
