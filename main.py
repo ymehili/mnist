0290e70b93d638b31ffdbc584a5dfae9e0489dc1
ymehili <youssefmehili@gmail.com>
2024-11-11 17:53:17 +0100

    refactor training loop to remove epoch iteration and streamline backpropagation process

--- main.py
+++ main.py
@@ -17,54 +17,49 @@ def main():
     epochs = 10
     learning_rate = 0.01
 
-    for epoch in range(epochs):
-        total_loss = 0
-        for index, (img, result) in enumerate(zip(img_train, result_train)):
-            inputs = img.flatten()
+    for index, (img, result) in enumerate(zip(img_train, result_train)):
+        inputs = img.flatten()
 
-            # Forward pass
-            input_activations = [neuron.activate(inputs) for neuron in input_neurons]
-            hidden_activations1 = [neuron.activate(input_activations) for neuron in hidden_neurons1]
-            hidden_activations2 = [neuron.activate(hidden_activations1) for neuron in hidden_neurons2]
-            output_activations = [neuron.activate(hidden_activations2) for neuron in output_neurons]
+        # Forward pass
+        input_activations = [neuron.activate(inputs) for neuron in input_neurons]
+        hidden_activations1 = [neuron.activate(input_activations) for neuron in hidden_neurons1]
+        hidden_activations2 = [neuron.activate(hidden_activations1) for neuron in hidden_neurons2]
+        output_activations = [neuron.activate(hidden_activations2) for neuron in output_neurons]
 
-            outputs = softmax(output_activations)
+        outputs = softmax(output_activations)
 
-            label = [0] * 10
-            label[result] = 1
+        label = [0] * 10
+        label[result] = 1
 
-            loss = cross_entropy_loss(outputs, label)
-            total_loss += loss
+        loss = cross_entropy_loss(outputs, label)
 
-            # Backpropagation
-            # Compute output layer error
-            output_errors = [output - label for output, label in zip(outputs, label)]
+        # Backpropagation
+        # Compute output layer error
+        output_errors = [output - label for output, label in zip(outputs, label)]
 
-            # Compute gradients for output layer
-            for i, neuron in enumerate(output_neurons):
-                for j in range(len(neuron.weights)):
-                    neuron.weights[j] -= learning_rate * output_errors[i] * hidden_activations2[j]
-                neuron.bias -= learning_rate * output_errors[i]
+        # Compute gradients for output layer
+        for i, neuron in enumerate(output_neurons):
+            for j in range(len(neuron.weights)):
+                neuron.weights[j] -= learning_rate * output_errors[i] * hidden_activations2[j]
+            neuron.bias -= learning_rate * output_errors[i]
 
-            # Compute hidden layer 2 error
-            hidden_errors2 = [sum(output_errors[k] * output_neurons[k].weights[i] for k in range(len(output_neurons))) for i in range(len(hidden_neurons2))]
+        # Compute hidden layer 2 error
+        hidden_errors2 = [sum(output_errors[k] * output_neurons[k].weights[i] for k in range(len(output_neurons))) for i in range(len(hidden_neurons2))]
 
-            # Compute gradients for hidden layer 2
-            for i, neuron in enumerate(hidden_neurons2):
-                for j in range(len(neuron.weights)):
-                    neuron.weights[j] -= learning_rate * hidden_errors2[i] * hidden_activations1[j]
-                neuron.bias -= learning_rate * hidden_errors2[i]
+        # Compute gradients for hidden layer 2
+        for i, neuron in enumerate(hidden_neurons2):
+            for j in range(len(neuron.weights)):
+                neuron.weights[j] -= learning_rate * hidden_errors2[i] * hidden_activations1[j]
+            neuron.bias -= learning_rate * hidden_errors2[i]
 
-            # Compute hidden layer 1 error
-            hidden_errors1 = [sum(hidden_errors2[k] * hidden_neurons2[k].weights[i] for k in range(len(hidden_neurons2))) for i in range(len(hidden_neurons1))]
+        # Compute hidden layer 1 error
+        hidden_errors1 = [sum(hidden_errors2[k] * hidden_neurons2[k].weights[i] for k in range(len(hidden_neurons2))) for i in range(len(hidden_neurons1))]
 
-            # Compute gradients for hidden layer 1
-            for i, neuron in enumerate(hidden_neurons1):
-                for j in range(len(neuron.weights)):
-                    neuron.weights[j] -= learning_rate * hidden_errors1[i] * input_activations[j]
-                neuron.bias -= learning_rate * hidden_errors1[i]
-
-        print(f"Epoch {epoch + 1}/{epochs}, Loss: {total_loss / len(img_train)}")
+        # Compute gradients for hidden layer 1
+        for i, neuron in enumerate(hidden_neurons1):
+            for j in range(len(neuron.weights)):
+                neuron.weights[j] -= learning_rate * hidden_errors1[i] * input_activations[j]
+            neuron.bias -= learning_rate * hidden_errors1[i]
 
     # Evaluate the model on the test dataset (to be implemented)
     # ...
